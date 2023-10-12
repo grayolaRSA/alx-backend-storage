c9adea4086cc58085bf9723e55652311611f6c95
root <gtndanga@gmail.com>
2023-10-12 18:01:58 +0000

    update

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,14 @@
+-- create a trigger to reset valid_email attribute
+-- after change of email
+
+DELIMITER //
+CREATE TRIGGER valid_email_reset
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+	IF NEW.email <> OLD.email THEN
+	   SET NEW.valid_email = 0;
+	END IF;
+END;
+//
+DELIMITER ;
