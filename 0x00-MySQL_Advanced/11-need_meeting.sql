aae823753818a58214a6f49b45580c3397fc085c
root <gtndanga@gmail.com>
2023-10-14 13:45:03 +0000

    update

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -3,5 +3,6 @@
 CREATE VIEW need_meeting AS
 SELECT name
 FROM students
-WHERE (score < 80 AND
-(last_meeting > DATE_SUB(NOW(), INTERVAL 1 MONTH) OR last_meeting IS NULL));
+-- WHERE (score < 80 AND
+-- (last_meeting > DATE_SUB(NOW(), INTERVAL 1 MONTH) OR last_meeting IS NULL));
+WHERE score < 80 AND (last_meeting IS NULL OR last_meeting > DATE_SUB(NOW(), INTERVAL 1 MONTH));
