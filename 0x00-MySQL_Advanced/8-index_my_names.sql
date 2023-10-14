ca995b794b739348d5e00940067a58094873a0b4
root <gtndanga@gmail.com>
2023-10-14 13:01:12 +0000

    update

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,11 +1,8 @@
--- script to create an index on table names
--- on column name
-
--- add column for first letter
-ALTER TABLE names ADD COLUMN first_letter CHAR(1);
-UPDATE names SET first_letter = SUBSTRING(name, 1, 1);
--- create index
-CREATE INDEX idx_name_first ON names (
--- select first character of name
-first_letter
-);
+-- Script to create an index on the `names` table
+-- SELECT SUBSTRING(name, 1, 1) AS first_letter FROM names;
+-- SELECT name INTO first_letter FROM names;
+-- SELECT LEFT(name, 1) AS first_letter
+-- FROM names;
+-- Create a computed (generated) column for the first letter
+ALTER TABLE names ADD COLUMN first_letter CHAR(1) GENERATED ALWAYS AS (LEFT(name, 1)) STORED;
+CREATE INDEX idx_name_first ON names (name);
